e5fbad2136a0f302935660847035ad3b7038ee4c
Toromo A <toromo.ade06@gmail.com>
2022-09-12 00:20:14 -0500

    changes

--- src/components/Section6.jsx
+++ src/components/Section6.jsx
@@ -4,17 +4,17 @@ import { NavLink } from "react-router-dom";
 const Section6 = () => {
   return (
     <div className='bg-[#F5F5F5]'>
-        <div className='lg:w-[1440px] w-[375px] m-auto '>
-        <div className='lg:w-[838px] w-[331px] m-auto h-[266px] flex flex-col justify-center content-center'>
+        <div className='lg:w-[1440px] w-[375px] m-auto lg:h-[618px] h-[378px]'>
+        <div className='lg:w-[838px] w-[331px] m-auto  flex flex-col justify-center content-center lg:py-[176px] py-[84px]'>
             <h1 className='lg:text-[46px] text-[24px] font-bold text-center leading-tight'>Embrace the power of the new corporate banking.</h1>
-            <p className='font-PublicSans text-center lg:text-[20px] text-[14px]'> Innovative corporate banking for accessible and convenient solutions that meets the criteria for future growth.  </p>
-            <div className='flex flex-col justify-center'>
-            <button
-              type="button"
-              className="text-sm rounded-lg text-white bg-[#6772E5] w-[169px] h-[50px] "
-            >
-              <NavLink to="/ready">Get Started</NavLink>
-            </button>
+            <p className='font-PublicSans text-center lg:text-[20px] text-[14px] lg:my-[48px] my-[24px] '> Innovative corporate banking for accessible and convenient solutions that meets the criteria for future growth.  </p>
+            <div className='relative left-0 right-0 m-auto'>
+                <button
+                type="button"
+                className="text-sm rounded-lg text-white bg-[#6772E5] w-[169px] h-[50px] "
+                >
+                <NavLink to="/ready">Get Started</NavLink>
+                </button>
             </div>
         </div>
         </div>
