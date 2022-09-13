e1fd12887a29361840e85cbff4dae415d8dd5dac
Toromo A <toromo.ade06@gmail.com>
2022-09-13 07:55:45 -0500

    product

--- src/components/Products/ProductHero.jsx
+++ src/components/Products/ProductHero.jsx
@@ -8,7 +8,7 @@ function ProductHero() {
                 <p className='lg:w-[1190px] w-[334px] text-[30px] font-bold lg:text-[56px] pt-[110px]  lg:text-center m-auto'>eGator is the spending solution you’ve been waiting for!</p>
                 <p className='lg:w-[896px] w-[326px] text-[#000505] m-auto lg:text-center pt-[36px] ' >Africa’s first corporate card and Expense Management Solution. Eliminate manual and complicated reimbursement processes and get real time visibility on your company spending.</p>
 
-                <img src={productHero} className='  flex  m-auto w-[250px] h-[139px] mt-[40px] lg:mt-[88px] lg:h-[280px] lg:w-[560px]' alt="" />
+                <img src="https://res.cloudinary.com/egator-finance-ltd/image/upload/v1663060183/eGator%20Website%20Assets/productHero_d5kqcg.png" className='  flex  m-auto w-[250px] h-[139px] mt-[40px] lg:mt-[88px] lg:h-[280px] lg:w-[560px]' alt="" />
             </div>
 
             <div className="bg-[#6772E5] relative h-[440px] lg:h-[493px] text-white ">
@@ -17,9 +17,9 @@ function ProductHero() {
                     suitable for Large Corporates, Startups, SMEs, NGOs and Governments. </p>
             </div>
             <div className="lg:flex justify-center lg:space-y-0 space-y-[24px] w-[255px] m-auto lg:w-[1274px] relative  -top-[80px] ">
-                <img className='lg:w-[414px] h-[157px] w-[255px] lg:h-[255px]' src={product1} alt="conference-hall" />
-                <img className='lg:w-[414px] h-[157px] w-[255px] lg:h-[255px]' src={product2} alt="conference-hall" />
-                <img className='lg:w-[414px] h-[157px] w-[255px] lg:h-[255px]' src={product3} alt="doctor&patient" />
+                <img className='lg:w-[414px] h-[157px] w-[255px] lg:h-[255px]' src='https://res.cloudinary.com/egator-finance-ltd/image/upload/v1663060177/eGator%20Website%20Assets/product1_hk1e61.png' alt="conference-hall" />
+                <img className='lg:w-[414px] h-[157px] w-[255px] lg:h-[255px]' src='https://res.cloudinary.com/egator-finance-ltd/image/upload/v1663060191/eGator%20Website%20Assets/product2_dtheee.png' alt="conference-hall" />
+                <img className='lg:w-[414px] h-[157px] w-[255px] lg:h-[255px]' src='https://res.cloudinary.com/egator-finance-ltd/image/upload/v1663060172/eGator%20Website%20Assets/product3_wrsx4h.png' alt="doctor&patient" />
             </div>
         </div>
 
