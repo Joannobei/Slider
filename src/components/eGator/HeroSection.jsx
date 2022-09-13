2f0274d9ce47cb4a8c7b2f4b310c6b20e4a8fed1
Toromo A <toromo.ade06@gmail.com>
2022-09-13 01:46:01 -0500

    fixes

--- src/components/eGator/HeroSection.jsx
+++ src/components/eGator/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
   return (
     <div className="bg-[#F3F4F6] ">
       <div className="pt-[190px] lg:max-w-[1440px] max-w-[375px] m-auto">
-        <div className="flex lg:flex-row justify-between flex-col items-center lg:w-[1250px] m-auto ">
+        <div className="flex lg:flex-row justify-between flex-col items-center lg:w-[1300px] m-auto ">
           <div className="lg:w-[519px] md:w-[419px] lg:m-0 w-[331px] m-auto lg:text-left text-center">
             <h1 className="lg:text-[56px] text-[30px] text-[#000505] lg:w-[519px] w-[302px] font-bold lg:h-[128px] leading-none ">
               Control spending, save time & money
