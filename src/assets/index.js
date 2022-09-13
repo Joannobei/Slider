62966dbad8c0d0db274443547372eb49ec4459d7
Toromo A <toromo.ade06@gmail.com>
2022-09-13 08:02:05 -0500

    changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,24 +1,7 @@
 import Logo from "./logo.png";
 import LogoFooter from "./logoFooter.png";
-import productHero from "./productHero.png";
-import product1 from "./product1.png";
-import product2 from "./product2.png";
-import product3 from "./product3.png";
-import productComputer from "./productComputer.png";
-import productCards from "./productCards.png";
-import productPhoneCard from "./productPhoneCard.png";
-
 
 export {
   Logo,
   LogoFooter,
-  productHero,
-  product1,
-  product2,
-  product3,
-  productCards,
-  productComputer,
-  productPhoneCard,
-  
-  
 };
