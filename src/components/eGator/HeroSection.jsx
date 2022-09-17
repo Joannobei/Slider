837abb789592000522b81c259b2167abcc0a2321
Toromo A <toromo.ade06@gmail.com>
2022-09-17 00:07:13 -0500

    biopay

--- src/components/eGator/HeroSection.jsx
+++ src/components/eGator/HeroSection.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Herocard } from "../../assets";
 import { NavLink } from "react-router-dom";
 
 const HeroSection = () => {
   return (
-    <div className="bg-[#F3F4F6] ">
-      <div className="pt-[190px] lg:max-w-[1440px] max-w-[375px] m-auto">
-        <div className="flex lg:flex-row justify-between flex-col items-center lg:w-[1300px] m-auto ">
+    <div className="bg-[#F3F4F6] w-full">
+      <div className="pt-[190px] lg:max-w-[1440px] max-w-375px] lg:w-[1440px] w-[375px] m-auto">
+        <div className="flex lg:flex-row justify-between flex-col items-center lg:w-[1300px] w-[320px] m-auto ">
           <div className="lg:w-[519px] md:w-[419px] lg:m-0 w-[331px] m-auto lg:text-left text-center">
             <h1 className="lg:text-[56px] text-[30px] text-[#000505] lg:w-[519px] w-[302px] font-bold lg:h-[128px] leading-none ">
               Control spending, save time & money
