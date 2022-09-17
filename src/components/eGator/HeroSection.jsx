78260ed8329743cf0d6ce4f98ff3ec88767133c2
Toromo A <toromo.ade06@gmail.com>
2022-09-17 00:16:15 -0500

    ad

--- src/components/eGator/HeroSection.jsx
+++ src/components/eGator/HeroSection.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 
 const HeroSection = () => {
   return (
-    <div className="bg-[#F3F4F6] w-full">
-      <div className="pt-[190px] lg:max-w-[1440px] max-w-375px] lg:w-[1440px] w-[375px] m-auto">
+    <div className="bg-[#F3F4F6] ">
+      <div className="pt-[190px]   m-auto">
         <div className="flex lg:flex-row justify-between flex-col items-center lg:w-[1300px] w-[320px] m-auto ">
           <div className="lg:w-[519px] md:w-[419px] lg:m-0 w-[331px] m-auto lg:text-left text-center">
             <h1 className="lg:text-[56px] text-[30px] text-[#000505] lg:w-[519px] w-[302px] font-bold lg:h-[128px] leading-none ">
@@ -23,7 +23,7 @@ const HeroSection = () => {
           </div>
 
           <div className="lg:w-[617.77px] w-[288.87px] ">
-            <img src='https://res.cloudinary.com/egator-finance-ltd/image/upload/v1663060203/eGator%20Website%20Assets/herocard_opi5ms.png' alt="hero_card" className="object-contain" />
+            <img src='https://res.cloudinary.com/egator-finance-ltd/image/upload/v1663060203/eGator%20Website%20Assets/herocard_opi5ms.png' alt="hero_card" className="object-contain lg:w-[617.77px] w-[288.87px]" />
           </div>
         </div>
       </div>
