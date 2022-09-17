d054ab20a1cceb81361dc707351e0c65ea83d903
Joan Nobei <nobeijoan1@gmail.com>
2022-09-17 06:31:28 -0100

    feat:404 page done

--- src/components/eGator/Section6.jsx
+++ src/components/eGator/Section6.jsx
@@ -8,7 +8,7 @@ const Section6 = () => {
           <div className='lg:w-[838px] w-[331px] m-auto flex flex-col justify-center content-center lg:py-[176px] py-[84px]'>
               <h1 className='lg:text-[46px] text-[24px] font-bold text-center leading-tight'>Embrace the power of the new corporate banking.</h1>
               <p className='font-PublicSans text-center lg:text-[20px] text-[14px] lg:my-[48px] my-[24px] '> Take the leap, take charge, use eGator and experience the power and efficiency of the new corporate banking. </p>
-              <div className='relative left-0 right-0 m-auto '>
+              <div className='  m-auto '>
                   <button
                   type="button"
                   className="text-sm rounded-md text-white bg-[#6772E5] w-[169px] h-[50px]">
